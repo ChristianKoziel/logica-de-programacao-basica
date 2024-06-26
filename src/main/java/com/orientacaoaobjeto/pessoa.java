14ff632c5a056f65df8d5c33c9a4b7b5d6e89521
Christian Koziel <christiankoziel@hotmail.com>
2024-06-25 22:47:35 -0300

    Primeiro commit

---
+++ src/main/java/com/orientacaoaobjeto/pessoa.java
@@ -0,0 +1,41 @@
+package com.orientacaoaobjeto;
+
+import java.util.Date;
+
+public class pessoa {
+    private String nome;
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+
+    public void setDataDeNascimento(Date dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
+    }
+
+    private Date dataDeNascimento;
+    private String sexo;
+
+    void mudarNome(String novoNome){
+        nome = novoNome;
+    }
+    void casar(String antigoSobrenome, String novoSobrenome) {
+        nome = nome.replace(antigoSobrenome, novoSobrenome);
+    }
+} 
